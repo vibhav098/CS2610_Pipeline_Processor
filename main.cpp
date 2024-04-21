3ea1b97f904d855c8b4c22d7f0b245fb9c25216b
BhargavGanesh0702 <167525851+BhargavGanesh0702@users.noreply.github.com>
2024-04-21 21:27:05 +0530

    main.cpp

--- main.cpp
+++ main.cpp
@@ -2,11 +2,23 @@
 //  pipeline.cpp
 //  Coding
 //
-//  Created by vibhav kanakamedala on 12/04/24.
+//  Created by vibhav kanakamedala and Bhargav Ganesh on 12/04/24.
 //
 #include <bits/stdc++.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
 using namespace std;
 
+void print(int8_t n,ofstream &output){
+    if(n<0){
+        int k = 256+n;
+        output<<hex<<k+0<<endl;
+        return;
+    }
+    output<<hex<<setw(2)<<setfill('0')<<n+0<<endl;
+
+}
 
 int ICache[256],PC=0;
 int8_t RF[16],DCache[256];
@@ -20,7 +32,7 @@ int twoscomplement(int n)
 {
     // array to store binary number
     vector<int> binaryNum(4);
-
+    int k = n;
     // counter for binary array
     int i = 0;
     while (n > 0) {
@@ -31,10 +43,14 @@ int twoscomplement(int n)
         i++;
     }
     if(binaryNum[3]==1){
-        return n-16;
+        return k-16;
     }
-    return n;
+    return k;
 }
+
+
+
+
 
 class Instruction {
 private:
@@ -85,7 +101,7 @@ public:
                 reg[IR[1]] = false;
                 break;
             case 7: //NOT instruction
-                if(is_ready(rs1, IR[1])) return;
+                if(is_ready(rs1, IR[2])) return;
                 reg[IR[1]] = false;
                 break;
             case 8: //SLLI instruction
@@ -96,7 +112,7 @@ public:
                 break;
             case 11: //LD instruction
                 if(is_ready(rs1, IR[2])) return;
-                Imm = (IR[3]);
+                Imm = twoscomplement(IR[3]);
                 reg[IR[1]] = false;
                 break;
             case 10: //LI instruction
@@ -106,7 +122,7 @@ public:
             case 12: //ST instruction
                 if(is_ready(rs1, IR[1])) return;
                 if(is_ready(rs1, IR[2])) return;
-                Imm = (IR[3]);
+                Imm = twoscomplement(IR[3]);
                 break;
             case 13: //JMP instruction
                 Imm = (IR[1]<<4)+(IR[2]);
@@ -126,70 +142,72 @@ public:
     
     void execute(){
         switch (IR[0]) {
-            case 0:
+            case 0://ADD
                 ALUOuptut = rs1 + rs2;
                 reg[IR[1]] = 0;
                 break;
-            case 1:
+            case 1://SUB
                 ALUOuptut = rs1 - rs2;
                 reg[IR[1]] = 0;
                 break;
-            case 2:
+            case 2://MUL
                 ALUOuptut = rs1*rs2;
                 reg[IR[1]] = 0;
                 break;
-            case 3:
+            case 3://INC
                 ALUOuptut = rs1 + 1;
                 reg[IR[1]] = 0;
                 break;
-            case 4:
+            case 4://AND
                 ALUOuptut = rs1 & rs2;
                 reg[IR[1]] = 0;
                 break;
-            case 5:
+            case 5://OR
                 ALUOuptut = rs1 | rs2;
                 reg[IR[1]] = 0;
                 break;
-            case 6:
+            case 6://XOR
                 ALUOuptut = rs1^rs2;
                 reg[IR[1]] = 0;
                 break;
-            case 7:
-                ALUOuptut = ~rs1;
+            case 7://NOT
+                
+                ALUOuptut = -1*(rs1+1);
+              
                 reg[IR[1]] = 0;
                 break;
-            case 8:
+            case 8://SLLI
                 ALUOuptut = rs1 << Imm;
                 reg[IR[1]] = 0;
                 break;
-            case 9:
+            case 9://SRLI
                 ALUOuptut = rs1 >> Imm;
                 reg[IR[1]] = 0;
                 break;
-            case 10:
+            case 10://LI
                 ALUOuptut = Imm;
                 reg[IR[1]] = 0;
                 break;
-            case 11:
+            case 11://LD
                 ALUOuptut = rs1 + Imm;
                 reg[IR[1]] = 0;
                 break;
-            case 12:
+            case 12://ST
                 ALUOuptut =rs1 + Imm;
                 break;
-            case 13:
+            case 13://JMP
                 Imm8 = Imm;
                 ALUOuptut = IPC + Imm8*2;
                 PC = ALUOuptut;
 //                can_fetch = 1;
                 break;
-            case 14:
+            case 14://BEQZ
                 Imm8 = Imm;
                 ALUOuptut = IPC + Imm8*2;
                 if(!rs1) PC = ALUOuptut;
 //                can_fetch = 1;
                 break;
-            case 15:
+            case 15://HLT
                 break;
         }
         stage++;
@@ -297,11 +315,87 @@ void read_from_file2(int8_t arr[],int size,string name){
 
 
 
-int main(){
+int main(int argc,char* argv[]){
+     int option_read;
+ string infolder,outfolder;
+  const option long_options[] =
+    {
+      /* These two options set a common flag to either 0 or 1. */
+      
+      {"in",   required_argument, 0, 'i'},
+      
+      {"out",  required_argument, 0, 'o'},
+      
+      
+      {0, 0, 0, 0}
+    };
+
+  while (1)
+    {
+      /* getopt_long stores the option index here. */
+      int option_index = 0;
+
+      option_read = getopt_long (argc, argv, "i:o:",
+                       long_options, &option_index);
+
+      /* Detect the end of the options. */
+      if (option_read == -1)
+        break;
+
+      switch (option_read)
+        {
+        
+
+        case 'i':
+          infolder = optarg;
+          break;
+
+        case 'o':
+          outfolder = optarg;
+          break;
+        
+        
+
+        case '?':
+          /* getopt_long already printed an error message. */
+          break;
+
+        default:
+          abort ();
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+    string inpICache = infolder + "/ICache.txt";
+    string inpDCache = infolder + "/DCache.txt";
+    string inpRF = infolder + "/RF.txt";
+    string outOutput = outfolder + "/Output.txt";
+    string outDcache = outfolder + "/DCache.txt";
+    string outRF = outfolder + "/RF.txt";
+
+
+
+
+
+
+
+
+
+
+
+    
     queue<Instruction> INS;
-    read_from_file(ICache, 256, "input/ICache.txt");
-    read_from_file2(DCache, 256, "input/DCache.txt");
-    read_from_file2(RF, 16, "input/RF.txt");
+    read_from_file(ICache, 256, inpICache);
+    read_from_file2(DCache, 256,inpDCache);
+    read_from_file2(RF, 16, inpRF);
     for(int i = 0; i < 16; i++)
         reg[i] = true;
     
@@ -335,38 +429,30 @@ int main(){
     }while(!INS.empty());
     
     ofstream output;
-    output.open("output/Output.txt");
-    output << "Total number of instructions executed    : " << num_inst << endl;
-    output << "Number of instructions in each class" << endl;
-    output << "Arithmetic instructions                  : " << num_arith << endl;
-    output << "Logical instructions                     : " << num_log << endl;
-    output << "Shift instructions                       : " << num_shift << endl;
-    output << "Memory instructions                      : " << num_store << endl;
-    output << "Load immediate instructions              : " << num_load << endl;
-    output << "Control instructions                     : " << num_con << endl;
-    output << "Halt instructions                        : " << num_halt << endl;
-    output << "Cycles Per Instruction                   : " << (double)num_cycles / num_inst << endl;
-    output << "Total number of stalls                   : " << num_stall << endl;
-    output << "Data stalls (RAW)                        : " << num_dat_st << endl;
-    output << "Control stalls                           : " << num_con_st << endl;
+    output.open(outOutput);
+    output << "Total number of instructions executed        : " << num_inst << endl;
+    output << "Number of instructions in each class     " << endl;
+    output << "Arithmetic instructions                      : " << num_arith << endl;
+    output << "Logical instructions                         : " << num_log << endl;
+    output << "Shift instructions                           : " << num_shift << endl;
+    output << "Memory instructions                          : " << num_store << endl;
+    output << "Load immediate instructions                  : " << num_load << endl;
+    output << "Control instructions                         : " << num_con << endl;
+    output << "Halt instructions                            : " << num_halt << endl;
+    output << "Cycles Per Instruction                       : " << (double)num_cycles / num_inst << endl;
+    output << "Total number of stalls                       : " << num_stall << endl;
+    output << "Data stalls (RAW)                            : " << num_dat_st << endl;
+    output << "Control stalls                               : " << num_con_st << endl;
     output.close();
     
-    output.open("output/DCache.txt");
+    output.open(outDcache);
     for(int i=0;i<256;i+=1){
-        if(DCache[i]>=16){
-            output<<hex<<DCache[i]+0<<endl;}
-        else{
-            output<<0<<hex<<DCache[i]+0<<endl;
-        }
+        print(DCache[i],output);
     }
     output.close();
-    output.open("output/RF.txt");
+    output.open(outRF);
     for(int i=0;i<16;i+=1){
-        if(RF[i]>=16){
-            output<<hex<<RF[i]+0<<endl;}
-        else{
-            output<<0<<hex<<RF[i]+0<<endl;
-        }
+        print(RF[i],output);
     }
 }
 
